4151469e2566c99dfa5c61081bc8404e6c2c2974
Rimantas <belauzas@yahoo.com>
2022-06-29 15:42:11 +0300

    Strings

---
+++ js/strings.js
@@ -0,0 +1,62 @@
+console.clear();
+/*
+STRINGS
+
+tekstas - simboliu kratinys/sarasas/masyvas
+
+teksto iniciavimas:
+- dvygubos kabutes (")
+- viengubos kabutes (')
+- backtick (apostrofas) (`)
+*/
+
+const name = 'Petras';
+console.log(name);
+
+const lastname = "Petraitis";
+console.log(lastname);
+
+const sentense = "Labas rytas Lietuva! 🟨🟩🟥";
+console.log(sentense);
+
+const p = "Labas rytas, labas rytas, labas rytas, labas rytas, labas rytas, labas rytas, labas rytas, labas rytas, labas rytas, labas rytas, labas rytas, labas rytas, labas rytas, labas rytas, labas rytas, labas rytas, labas rytas, labas rytas, labas rytas. Ka tu?";
+console.log(p);
+
+const s1 = "Vienguba kabute(').";
+console.log(s1);
+
+const s2 = 'Dvyguba kabute(").';
+console.log(s2);
+
+const s12_1 = 'Vienguba kabute(\') ir dvyguba kabute(\").';
+console.log(s12_1);
+const s12_2 = "Vienguba kabute(\') ir dvyguba kabute(\").";
+console.log(s12_2);
+
+const slash1 = 'Slash (\\).';
+console.log(slash1);
+
+const slash2 = "Slash (\\\\).";
+console.log(slash2);
+
+const enter = 'Labas\r\n\
+rytas\r\n\
+Lietuva.';
+console.log(enter);
+
+const back1 = `Vienguba kabute(').`;
+console.log(back1);
+
+const back2 = `Dviguba kabute(").`;
+console.log(back2);
+
+const back12 = `Vienguba kabute(') ir dvyguba kabute(").`;
+console.log(back12);
+
+const back123 = `Vienguba ('), dviguba (") ir backtick (\`) kabutes.`;
+console.log(back123);
+
+const backEnter = `Labas
+rytas
+Lietuva.`;
+console.log(backEnter);
